28426b9f82c6f88c4d18abd86876ccdc0d9b1fb0
Melker Holmgren <melker@funnel.io>
2025-02-28 09:29:50 +0100

    rust learning: first steps

--- data-types/src/main.rs
+++ data-types/src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+fn main() {
+
+    println!("Every Rust value has a data type");
+    println!(
+        "Rust is a statically typed language, 
+        which means that the compiler must know the types of all 
+        variables at compile time."
+    );
+    println!(
+        "Rust can infer the types of variables based on their initial assignments."
+    );
+
+    println!("The type of a string is a `&str`.");
+    println!("Data types fall in several different categories.");
+    println!("Scalar types: represent a single value.");
+    println!("Rust has four primary scalar types: integers, floating-point numbers, booleans, and characters.");
+    println!("An integer is a whole number like 2, 7, or -10. Rust has two types of integers: signed and unsigned.");
+    println!("Signed integers: can represent positive and negative values. They start with a `i`.");
+    println!("Unsigned integers: Only represent positive values and zero. They start with a `u`.");
+    println!("A floating-point number is a number with a decimal point, like 2.5 or -1.75.");
+    
+}
